18b17a49fcf7e9f833b404ad9dd7a77c6a51f745
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2024-03-04 13:48:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzBuzz00;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        string fizz = "Fizz";
+        string buzz = "Buzz";
+        string fb = "FizzBuzz";
+
+        int number = 100;
+
+        for (int i = 0; i < number; i++)
+        {
+            if (i % 3 == 0 )
+            {
+                Console.WriteLine(fizz);
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine(buzz);
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine(fb);
+            }
+            else  Console.WriteLine(i);
+        }
+        
+        
+    }
+}
